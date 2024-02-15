c7140d1e959fad1926ea8f8f84d5eb7553f1dbae
SleimaD <sleima@icloud.com>
2024-02-15 11:19:23 +0100

    add of display messages

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -16,6 +16,21 @@ export default function Result(props) {
 
   return (
     <div className="result-container mt-[7rem] flex text-white justify-center items-center gap-[10rem]">
+        <div className="choice-container flex flex-col justify-normal items-center gap-5">
+            <p>You picked</p>
+            <div>{props.userChoice}</div> 
+        </div>
+
+        <div className="result-message flex flex-col justify-normal items-center gap-5">
+            <p>  You {props.choices[props.userChoice].beats === props.computerChoice ? 'Win' : 'Lose'}!</p>
+            
+            <button className=' bg-white text-black p-2 px-10 rounded-xl' onClick={props.playAgain}>PLAY AGAIN</button>
+        </div>
+
+        <div className="choice-container flex flex-col justify-normal items-center gap-5">
+            <p>The house picked</p>
+            <div>{props.computerChoice}</div> 
+        </div>
   </div>
 
   )
