39051baca0c7d4787ff6047ed4c4ed212b4e6296
SleimaD <sleima@icloud.com>
2024-02-15 09:34:06 +0100

    add of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Title from './components/Title/Title'
 
 function App() {
   
   return (
-    <div className="">
+    <div className="App w-full h-[100vh] flex  items-center flex-col">
+      <Title/>
     </div>
   )
 }
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className=" border-gray-500 border-[2px] mt-8 flex justify-between p-5 w-[40%] rounded-xl">
+    </div>
+  )
+}
